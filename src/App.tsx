00f1c273d5ddf7b5089e0063c76cf3914fffde4f
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2024-12-27 10:53:22 -0300

    implementação da feature do controle

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,62 @@ import { useEffect, useState } from "react"
 function App() {
   const [vermelho, setVermelho] = useState(0);
   const [azul, setAzul] = useState(0);
+  const [gamepadConnected, setGamepadConnected] = useState(false);
 
-  const handleKeyPress = (event: KeyboardEvent) => {
-    switch (event.key) {
-      case 'q':
+  const handleGamepadInput = () => {
+    const gamepads = navigator.getGamepads();
+
+    if (gamepads[0]) {
+      const gamepad = gamepads[0];
+
+      if (gamepad.buttons[0].pressed) {
         contarVermelho2();
-        break;
-      case 'u':
+      }
+
+      if (gamepad.buttons[1].pressed) {
         contarAzul2();
-        break;
-      case 'w':
+      }
+
+      if(gamepad.buttons[2].pressed) {
         contarVermelho3();
-        break;
-      case 'i':
+      }
+
+      if(gamepad.buttons[3].pressed) {
         contarAzul3();
-        break;
-      case 'n':
+      }
+
+      if (gamepad.buttons[12].pressed) {
         zerarPontos();
-        break;
+      }
+    }
+  };
+
+  const gamepadListener = (e: GamepadEvent) => {
+    if (e.type === "gamepadconnected") {
+      setGamepadConnected(true);
+      console.log("Gamepad conectado:", e.gamepad.id);
+    } else if (e.type === "gamepaddisconnected") {
+      setGamepadConnected(false);
+      console.log("Gamepad desconectado:", e.gamepad.id);
     }
   };
 
   useEffect(() => {
-    document.addEventListener('keydown', handleKeyPress);
+    window.addEventListener("gamepadconnected", gamepadListener);
+    window.addEventListener("gamepaddisconnected", gamepadListener);
+
+    const interval = setInterval(() => {
+      if (gamepadConnected) {
+        handleGamepadInput();
+      }
+    }, 120);
+
     return () => {
-      document.removeEventListener('keydown', handleKeyPress);
+      window.removeEventListener("gamepadconnected", gamepadListener);
+      window.removeEventListener("gamepaddisconnected", gamepadListener);
+      clearInterval(interval);
     };
-  }, []);
+  }, [gamepadConnected]);
 
   function contarVermelho2() {
     setVermelho((prevVermelho) => prevVermelho + 2);
@@ -54,15 +83,18 @@ function App() {
 
   return (
     <div className="w-full h-screen">
+      <h1 className="text-center text-lg">
+        {gamepadConnected ? "Gamepad conectado!" : "Conecte um gamepad."}
+      </h1>
       <div className="w-full h-full grid grid-cols-2">
         <div className="bg-red-600 w-full grid col-span-1">
           <div className="justify-center items-center flex-col flex">
-          <span>{vermelho}</span>
+          <span className="text-9xl">{vermelho}</span>
           </div>
         </div>
         <div className="bg-blue-600 w-full col-span-1  grid">
           <div className="justify-center items-center flex-col flex">
-          <span>{azul}</span>
+          <span className="text-9xl">{azul}</span>
           </div>
         </div>
       </div>
